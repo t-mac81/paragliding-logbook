99030596da6e665a4f6b82620e6aedf794ac56b9
Trevor <macmurrt@hotmail.com>
2022-02-24 17:24:50 -0800

    updated react-scripts to 5.0.0, added eslint, airbnb style.

---
+++ src/.eslintrc.js
@@ -0,0 +1,47 @@
+module.exports = {
+    env: {
+        browser: true,
+    },
+    extends: [
+        'airbnb',
+        'airbnb-typescript',
+    ],
+    parserOptions: {
+        project: ['./tsconfig.json']
+    },
+    rules: {
+
+        // A list of allowed function definition syntaxes.
+        'react/function-component-definition': [2, {
+            'namedComponents': ['function-declaration', 'arrow-function'],
+        }],
+        // Default length is 100.
+        'max-len': 'off',
+        // Allow underscore as a prefix for private values.
+        'no-underscore-dangle': 'off',
+
+        // Allow hoisting.
+        'no-use-before-define': 'off',
+        '@typescript-eslint/no-use-before-define': 'off',
+
+        // Allow amplify _version to be number.
+        '@typescript-eslint/naming-convention': 'off',
+        'react/jsx-props-no-spreading': [2,
+            {
+                'exceptions': ['IonTextarea', 'IonInput']
+            }],
+        // This rule disallows you to use more recent javascript features.
+        // We don't need this rule because webpack + babel can handle it.
+        'no-restricted-syntax': 'off',
+
+        // Use single quotes in jsx.
+        'jsx-quotes': [2, 'prefer-single'],
+        // Let's not use explicit any.
+        '@typescript-eslint/no-explicit-any': [2, {
+            'fixToUnknown': false,
+            'ignoreRestArgs': false
+        }],
+        // Since React v17, it's not required to import React.
+        'react/react-in-jsx-scope': 'off',
+    },
+};
